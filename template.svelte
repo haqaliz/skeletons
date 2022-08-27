8711a2d23d406a491e5ece340298eb410689658e
Ali Haqiqi <aliz@foresightanalytics.ca>
2022-08-27 23:52:21 +0430

    vim skeletons
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

---
+++ template.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<!-- TEMPLATE -->
+
+<style lang="scss">
+</style>
---
+++ template.vue
@@ -0,0 +1,23 @@
+<template>
+	
+</template>
+
+<script>
+	export default {
+		name: 'NAME',
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			
+		},
+	};
+</script>
+
+<style scoped lang='scss'>
+
+
+
+</style>
